2abb9e03dfe45240767b5028d4c4c60cd12578a1
arshab17 <arshab98@gmail.com>
2023-12-13 18:26:49 +0530

    Removed directory

--- OneDrive/Desktop/Ecommerce/HighStreet/store/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from.views import *
-urlpatterns = [
-    path('',home,name='home'),
-    path('store/<slug:category_slug>/',stores,name='products_by_category'),
-    path('store/<slug:category_slug>/<slug:product_slug>',product_details,name='product_details'),
-    path('submit_review/<int:product_id>/',submit_review,name="submit_review")
-]
